76cd1e08e9eeb4133b7d3721f02081283c942482
Thomas Leese <thomas@leese.io>
2019-01-09 18:51:52 +0000

    Release

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60dc2efd7a1f4c8342a459e4a34301cf.js"
+  "/precache-manifest.ca3a9a18753dddc82d1818ac868cc044.js"
 );
 
 workbox.clientsClaim();
