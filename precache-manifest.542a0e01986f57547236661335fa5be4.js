f0b80e04dfcb908cfae6d28214a0ad3e1e878963
vikash tank <vikashtank@hotmail.co.uk>
2019-02-18 21:43:05 +0000

    release update

---
+++ precache-manifest.542a0e01986f57547236661335fa5be4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1dceb660319cfab85d0",
+    "url": "/static/css/main.4a0c1fb3.chunk.css"
+  },
+  {
+    "revision": "b1dceb660319cfab85d0",
+    "url": "/static/js/main.b1dceb66.chunk.js"
+  },
+  {
+    "revision": "b391a55ad5e238a7bbff",
+    "url": "/static/js/1.b391a55a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "59df145c1cf3f7a95782bc85f7c14499",
+    "url": "/static/media/workout_image.59df145c.png"
+  },
+  {
+    "revision": "5732f2b30d313e8bce143c9d18509249",
+    "url": "/static/media/ipad_image.5732f2b3.png"
+  },
+  {
+    "revision": "8296f14f0b58085376fe5805137cf4df",
+    "url": "/static/media/goals_image.8296f14f.png"
+  },
+  {
+    "revision": "1144c15fb41ddb606e785f836daf8d92",
+    "url": "/static/media/barbell.1144c15f.jpg"
+  },
+  {
+    "revision": "c32da4f7b7d21ac7e7f8795fa4f6587d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e85fdf369522e33777067e5d424ed1e.js"
+  "/precache-manifest.542a0e01986f57547236661335fa5be4.js"
 );
 
 workbox.clientsClaim();
